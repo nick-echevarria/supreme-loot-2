db44dbe67a27fbf54da0a89b424e61c854e1fd1b
nick-echevarria <nick.echev@gmail.com>
2021-07-21 15:33:21 +0200

    Added logger if statement

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import { persistStore } from "redux-persist";
 // Also allows us to familiarize ourselves to what middleware does
 import logger from "redux-logger";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 // Spreading middlewares value is scalable
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
