57dba7125e5cc64011ff54a3d09cf34adda84619
nick-echevarria <nick.echev@gmail.com>
2021-07-23 08:17:01 +0200

    Added function to create object shapes that we need

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -65,4 +65,18 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collection.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+};
+
 export default firebase;
