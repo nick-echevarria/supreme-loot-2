67ed830f5cf651f4c6c4cde09a83d3826adc5be9
nick-echevarria <nick.echev@gmail.com>
2021-07-28 14:05:17 +0200

    Trying to fix deployed homepage by replacing BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // Parent of everything in our app that allows access to store
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -11,11 +11,11 @@ import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="./">
+    <HashRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
