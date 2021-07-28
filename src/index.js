f095cba59da2c6c88311c688453280c3e648d57d
nick-echevarria <nick.echev@gmail.com>
2021-07-28 17:23:22 +0200

    Replaced HashRouter w/ BrowserRouter, deleted basename, and edited hompage attr in package.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 // Parent of everything in our app that allows access to store
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -11,11 +11,11 @@ import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter basename="/">
+    <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
