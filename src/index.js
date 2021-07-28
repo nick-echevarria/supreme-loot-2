889ff404dd439a959958b13412470fd2d54972d4
nick-echevarria <nick.echev@gmail.com>
2021-07-28 13:38:45 +0200

    Trying to fix deploy issue w/ rendering homepage by adding basename attribute

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
